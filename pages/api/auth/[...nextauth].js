ad771418f382b3ecd9b990c389feae5a94c85114
Gideon Shils <gideon.shils@gmail.com>
2021-02-27 16:20:50 -0500

    Add spotify auth

---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,13 @@
+import NextAuth from 'next-auth'
+import Providers from 'next-auth/providers'
+
+export default NextAuth({
+  providers: [
+    Providers.Spotify({
+        clientId: process.env.SPOTIFY_CLIENT_ID,
+        clientSecret: process.env.SPOTIFY_CLIENT_SECRET
+      })
+  ],
+
+  database: process.env.DB_URI,
+})
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,7 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default (req, res) => {
+import { NextApiRequest, NextApiResponse } from "next"
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
   res.status(200).json({ name: 'John Doe' })
 }
